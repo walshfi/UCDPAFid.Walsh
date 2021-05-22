557ce0426270968dea614fe377d611d0b8fea6e0
fidelmawalsh <fidelma_walsh@yahoo.com>
2021-05-22 18:35:44 +0100

    upload amended update of project

--- main.py
+++ main.py
@@ -33,4 +33,35 @@ print(SP_500_stockprices_symbol.tail())
 # Filter the SP 500 where the opening value is over 100
 SP_500_open_100 = SP_500_stockprices_symbol[SP_500_stockprices_symbol['open']>100]
 
+# Import CSV file into pandas for further analysis
+
+Bestsellers = pd.read_csv('bestsellers with categories.csv')
+
+# Print the file info for Bestsellers
+print(Bestsellers.info())
+
+# Using indexing print the first four rows from Bestsellers
+print(Bestsellers[0:4])
+
+# Extract the columns of authors and names from the data
+
+Bestsellers_Author = Bestsellers.iloc[:,0:2]
+print(Bestsellers_Author)
+
+# Drop any duplication of titles of books
+Bestsellers_Title = Bestsellers.drop_duplicates('Name').sort_values('Price', ascending=True)
+print(Bestsellers_Title.head(12))
+
+# Group the genre of books by fiction and non fiction and the average price
+
+Bestsellers_Genre_avg_price = Bestsellers.groupby('Genre')['Price'].mean()
+print(Bestsellers_Genre_avg_price)
+
+# Group the Bestsellers by Genre and find the min, max, and average of fiction and non fiction for both prices and reviews
+
+Bestsellers_stats = Bestsellers.groupby('Year')[['Price','Reviews']].agg([min,max,])
+print(Bestsellers_stats)
+
+
+
 
